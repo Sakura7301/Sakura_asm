4f4ee909b8ab0c43cff0ddb01b7b03ec43d4c49b
Sakura7301 <sakuraduck@foxmail.com>
2021-10-14 11:19:49 +0800

    add->32bit_sbb

---
+++ 02_add/05_32bit_sub.asm
@@ -0,0 +1,13 @@
+;bx:ax=0x00020003
+mov bx,0x0002
+mov ax,0x0003
+
+;dx:cx=0x00030002
+mov dx,0x0003
+mov cx,0x0002
+
+sub ax,cx
+sbb bx,dx
+;结果在bx:ax=0xffff0001
+times 510-($-$$) db 0
+db 0x55,0xaa
